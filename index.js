97bd46507206b556aa68f8c98c98f07f1547a2fd
Mishaaa2024 <mishafatimaali2013@gmail.com>
2024-05-11 11:52:07 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Mysha Fatima')
+  })
+
+  app.get('/login', (req, res) => {
+    res.send('<h1>Please Log in</h1.')
+  })
+  
+app.get('/chai',(req,res)=>{
+   res.send("This is a chai page")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
